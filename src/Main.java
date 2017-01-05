d7cadebce44b3659f03f76741e66181d813a365e
cys77135 <cys77135@ssu.ac.kr>
2017-01-05 10:24:07 +0900

    거리

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,21 @@
+
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("*");
-        System.out.println("**");
-        System.out.println("***");
-        System.out.println("****");
-        System.out.println("*****");
+        Scanner n = new Scanner(System.in);
+        double x1, x2, y1, y2;
+        double ans;
+        System.out.print("x1, y1 좌표 입력 : ");
+        x1 = n.nextDouble();
+        y1 = n.nextDouble();
+        System.out.print("x2, y2 좌표 입력 : ");
+        x2 = n.nextDouble();
+        y2 = n.nextDouble();
+
+        ans = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        System.out.printf("거리 : %f\n", ans);
     }
 }
