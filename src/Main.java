0058e06393fa63869cf23e1686cd3ad8e34f05b1
cys77135 <cys77135@ssu.ac.kr>
2017-01-05 10:43:35 +0900

    랜덤

--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,22 @@ public class Main
     public static void main(String[] args)
     {
         Scanner n = new Scanner(System.in);
-        double x1, x2, y1, y2;
-        double ans;
-        System.out.print("x1, y1 좌표 입력 : ");
-        x1 = n.nextDouble();
-        y1 = n.nextDouble();
-        System.out.print("x2, y2 좌표 입력 : ");
-        x2 = n.nextDouble();
-        y2 = n.nextDouble();
-
-        ans = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-        System.out.printf("거리 : %f\n", ans);
+        int ran = (int) (Math.random() * 100), num;
+        System.out.println(ran);
+        while (true)
+        {
+            num = n.nextInt();
+            if (num > ran)
+            {
+                System.out.println("높습니다.");
+            } else if (num < ran)
+            {
+                System.out.println("낮습니다.");
+            } else
+            {
+                System.out.println("정답입니다.");
+                break;
+            }
+        }
     }
 }
