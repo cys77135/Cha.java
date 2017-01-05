b410af0fa9311ed9b46ad7bf2f20bd4b6da0d207
BoxResin <boxresin@gmail.com>
2017-01-05 10:56:05 +0900

    Hello Java 출력

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,8 @@
 
-import java.util.Scanner;
-
 public class Main
 {
     public static void main(String[] args)
     {
-        Scanner n = new Scanner(System.in);
-        int ran = (int) (Math.random() * 100), num;
-        System.out.println(ran);
-        while (true)
-        {
-            num = n.nextInt();
-            if (num > ran)
-            {
-                System.out.println("높습니다.");
-            } else if (num < ran)
-            {
-                System.out.println("낮습니다.");
-            } else
-            {
-                System.out.println("정답입니다.");
-                break;
-            }
-        }
+        System.out.println("Hello Java!");
     }
 }
