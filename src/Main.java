cf93aac2c7285bfdc4128ab78d33f2a15c385b78
BoxResin <boxresin@gmail.com>
2017-01-05 11:16:39 +0900

    Minsuk Branch에 추가

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main
     public static void main(String[] args)
     {
         System.out.println("Minsuk Eom Edited!");
+        System.out.println("This is Minsuk Branch.");
     }
 }
