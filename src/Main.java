07b051447b406d8f4d88f7e6ce77ee9ca3c86891
cys77135 <cys77135@ssu.ac.kr>
2017-01-05 11:17:25 +0900

    별

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Minsuk Eom Edited!");
+        Scanner n = new Scanner(System.in);
+        int i, j, num = n.nextInt();
+        for (i = 1; i <= num; i++)
+        {
+            for (j = 1; j <= i; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
     }
 }
